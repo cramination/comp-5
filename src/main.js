0f3f94ff9cd6a33d17a512c087786a897013aeb4
cramination <camnation@comcast.net>
2025-02-04 11:40:38 -0500

    added video textures

--- src/main.js
+++ src/main.js
@@ -53,7 +53,162 @@ camera.position.set(3, .5, 6)
 window.addEventListener('resize', onWindowResize);
 onWindowResize();
 
+// Objects
 
+const video =  document.getElementById("film");
+const video3 =  document.getElementById("film3");
+const video4 =  document.getElementById("film4");
+const video5 =  document.getElementById("film5");
+const texture = new
+THREE.VideoTexture(video);
+const texture3 = new
+THREE.VideoTexture(video3);
+const texture4 = new
+THREE.VideoTexture(video4);
+const texture5 = new
+THREE.VideoTexture(video5);
+
+
+const geometry = new THREE.CylinderGeometry( 1, 1, 1, 32 ); 
+  const videoTexture = new THREE.VideoTexture(video);
+  const videoMaterial =  new THREE.MeshBasicMaterial( {
+    map: videoTexture, 
+    side: THREE.FrontSide, 
+    toneMapped: false,
+    transparent: true,
+    opacity: 0.9
+  } );
+  const cylinder = new THREE.Mesh( geometry, videoMaterial ); 
+  scene.add( cylinder );
+  cylinder.rotation.x += Math.PI / 2;
+  cylinder.position.set (.978, .67, 3.564)
+  cylinder.scale.set(.115, .13, .115)
+  videoTexture.repeat.set(0.6, 0.6);
+  videoTexture.offset.set(0.18, 0.18);
+
+ 
+  const geometry2 = new THREE.CylinderGeometry( 1, 1, 1, 32 ); 
+  const videoTexture2 = new THREE.VideoTexture(video3);
+  const videoMaterial2 =  new THREE.MeshBasicMaterial( {
+    map: videoTexture2,
+    side: THREE.FrontSide, 
+    toneMapped: false,
+    transparent: true,
+    opacity: 0.3 
+  } );
+  const cylinder2 = new THREE.Mesh( geometry2, videoMaterial2 ); 
+  scene.add( cylinder2 );
+  cylinder2.rotation.x += Math.PI / 2;
+  cylinder2.rotation.z += Math.PI / 2;
+  cylinder2.position.set (.607, .695, 3.72)
+  cylinder2.scale.set(.16, .07, .16)
+
+
+  const geometry3 = new THREE.CylinderGeometry( 1, 1, 1, 32 ); 
+  const videoTexture3 = new THREE.VideoTexture(video3);
+  const videoMaterial3 =  new THREE.MeshBasicMaterial( {
+    map: videoTexture3, 
+    side: THREE.FrontSide, 
+    toneMapped: false,
+    transparent: true,
+    opacity: 0.5
+  } );
+  const cylinder3 = new THREE.Mesh( geometry3, videoMaterial3 ); 
+  scene.add( cylinder3 );
+  cylinder3.rotation.x += Math.PI / 2;
+  cylinder3.rotation.z += Math.PI / 2;
+  cylinder3.position.set (1.71, .73, 3.72)
+  cylinder3.scale.set(.16, .07, .16)
+
+
+  const geometry4 = new THREE.CylinderGeometry( 1, 1, 1, 32 ); 
+  const videoTexture4 = new THREE.VideoTexture(video3);
+  const videoMaterial4 =  new THREE.MeshBasicMaterial( {
+    map: videoTexture4, 
+    side: THREE.FrontSide, 
+    toneMapped: false,
+    transparent: true,
+    opacity: 0.5
+  } );
+  const cylinder4 = new THREE.Mesh( geometry4, videoMaterial4 ); 
+  scene.add( cylinder4 );
+  cylinder4.rotation.x += Math.PI / 2;
+  cylinder4.rotation.z += Math.PI / 2;
+  cylinder4.position.set (1.71, .4, 3.72)
+  cylinder4.scale.set(.16, .07, .16)
+  
+
+  const geometry5 = new THREE.BoxGeometry( 1, 1, 1 ); 
+  const videoTexture10 = new THREE.VideoTexture(video5);
+  const videoTexture11 = new THREE.VideoTexture(video4);
+  const material5 = [
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture10, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture10, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture11, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture11, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture10, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture10, side: THREE.DoubleSide, toneMapped: false
+    } ),
+  ];
+  const cube = new THREE.Mesh( geometry5, material5 ); 
+  cube.position.set (1.203, .69, 3.465)
+  cube.scale.set(.029, .35, .029)
+  videoTexture10.repeat.set(0.6, 0.6);
+  scene.add( cube );
+
+
+
+  const geometry6 = new THREE.BoxGeometry( 1, 1, 1 ); 
+  const videoTexture7 = new THREE.VideoTexture(video5);
+  const videoTexture9 = new THREE.VideoTexture(video4);
+
+  const material6 = [
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture7, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture7, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture9, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture9, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture7, side: THREE.DoubleSide, toneMapped: false
+    } ),
+    new THREE.MeshBasicMaterial( {
+      map: videoTexture7, side: THREE.DoubleSide, toneMapped: false
+    } ),
+  ];
+  const cube2 = new THREE.Mesh( geometry6, material6 );
+  cube2.position.set (1.262, .69, 3.465)
+  cube2.scale.set(.029, .35, .029)
+  videoTexture7.repeat.set(0.6, 0.6);
+  videoTexture7.offset.set(0.52, 0.52);
+  scene.add( cube2 );
+
+  
+
+  const geometry7 = new THREE.PlaneGeometry( 1, 1, );
+  const videoTexture8 = new THREE.VideoTexture(video5);
+  const material7 = new THREE.MeshBasicMaterial( {map: videoTexture8, side: THREE.DoubleSide, toneMapped: false} );
+  const plane = new THREE.Mesh( geometry7, material7 );
+  plane.position.set (1.366, .219, 3.372)
+  plane.scale.set(.03, .205, .02)
+  scene.add( plane );
 
 const loader = new GLTFLoader();
 
